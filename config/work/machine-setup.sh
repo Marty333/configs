0dd357ff41fba174012e4fe6bb70c2f59be56449
Marty Sherrill <marty.sherrill@thermofisher.com>
2023-09-27 17:47:37 -0500

    Add Oracle client

--- config/work/machine-setup.sh
+++ config/work/machine-setup.sh
@@ -3,10 +3,12 @@
 
 PKG_MANAGER='apt-get'
 $PKG_MANAGER update && $PKG_MANAGER install -y \
+	alien\
 	git\
 	zsh\
 	htop\
 	curl\
+	vim\
 	wget\
 	bat\
 	exa\
@@ -15,6 +17,13 @@ $PKG_MANAGER update && $PKG_MANAGER install -y \
 	fzf\
 	silversearcher-ag\
 	zoxide
+
+# Install Oracle client:
+cd /tmp
+wget -q http://victoria.invitrogen.com/software/oracle/oracle-instantclient19.5-basic-19.5.0.0.0-1.x86_64.rpm
+wget -q http://victoria.invitrogen.com/software/oracle/oracle-instantclient19.5-devel-19.5.0.0.0-1.x86_64.rpm
+alien -i --scripts /tmp/oracle-instantclient19.5-basic-19.5.0.0.0-1.x86_64.rpm
+alien -i --scripts /tmp/oracle-instantclient19.5-devel-19.5.0.0.0-1.x86_64.rpm
 
 # Add my userID:
 useradd sherrimn
